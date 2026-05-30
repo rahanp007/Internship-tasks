c5911f310755b96a7b95635792ec8d1a2c302642
rahan.p <rahanp007@gmail.com>
2026-05-30 16:20:02 +0530

    Update Carclass.py

--- Carclass.py
+++ Carclass.py
@@ -4,7 +4,17 @@ class Car:
         self.model=model
         self.year=year
         self.odometer=0
-    def drive(self):
-        self.odometer=self.odometer+1
+    def drive(self,Km):
+        self.odometer=self.odometer+Km
     def get_info(self):
-        print(f"the car travelled {self.odometer} kms")
+    
+        print(f"The car travelled {self.odometer}km{"s" if self.odometer>1 else ""}")
+car1=Car("BMW","M4",2024)
+car2=Car("Benz","G-Wagon", 2024)
+car1.drive(1)
+car1.drive(0)
+car2.drive(10)
+car2.drive(0)
+print(f"The Car Covered {car1.odometer}km{"s" if car1.odometer>1 else ""}")
+car1.get_info()
+car2.get_info()
